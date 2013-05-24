c275a94c87fdc97572690f052ced50b6caa55575
Jhaura Wachsman <jw@jhaurawachsman.com>
2013-05-24 14:03:45 -0700

    Finishing up documentation.

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ The methods can be used on assets such as CSS and JS files, images, XML feeds in
 
 Transform a _root-relative_ URL into a _base-relative_ URL.
 
-In your template and optionally in your markdown files use the `to_baseurl` method to __append__ the `baseurl` parameter value in `_config.yml` to _root-relative_ URLs.
+In your templates and optionally in your Markdown files use the `to_baseurl` method to __append__ the `baseurl` parameter value in `_config.yml` to _root-relative_ URLs.
 
 Example:
 
@@ -50,7 +50,7 @@ When you _generate_ your site, the result is:
 
 Transform a _root-relative_ URL into an _absolute_ URL.
 
-In your template and optionally in your markdown files use the `to_absurl` method to __append__ the `@url` parameter value in `_config.yml` to _root-relative_ URLs.
+In your templates and optionally in your Markdown files use the `to_absurl` method to __append__ the `@url` parameter value in `_config.yml` to _root-relative_ URLs.
 
 Example:
 
@@ -69,6 +69,137 @@ When you _generate_ your site, the result is:
 If you also have `baseurl` set in `_config.yml` then `to_baseurl` will be run before and you'll get:
 
     <link href="http://www.domain.tld/blog/feed/index.xml" rel="self" />
+
+### method to_cdnurl
+
+Prepend a CDN 'virtual versioned' URL for any relative URL resource. Uses up to 4 hosts for multiple download streams at the same time.
+
+In your templates and optionally in your Markdown files use the `to_cdnurl` method to __append__ the `cdn_hosts` parameter value in `_config.yml` to _root-relative_ URLs.
+
+Jekyll's `_config.yml` does not have a `cdn_hosts` parameter, so we need to add our own first. There will likely be other _user specified_ parameters that you will need, so we can namespace all of them in a hash called `app`:
+
+    app:
+      release: 100000
+      prefix:
+      mode: development
+      cdn_hosts:
+        - xxxxxxxxxxxxx0.cloudfront.net
+        - xxxxxxxxxxxxx1.cloudfront.net
+        - xxxxxxxxxxxxx2.cloudfront.net
+        - xxxxxxxxxxxxx3.cloudfront.net
+
+You will also need a __RewriteRule__ in `.htaccess` to create the 'virtual version' match to the actual asset (assumes `prefix` is 'v'):
+
+    RewriteRule ^v[0-9]{6,6}/(.*)$ /$1 [L]
+
+Note: You do not actually have to rename any files or folders to use 'virtual versioned' URLs. You only need to make sure your 'href' and 'src' attributes point to the virtual version URL. The method does exactly that for you.
+
+Tip: You can _virtual version_ any asset or file just by linking to it using this method! That means PDFs, movies, images, index.html files and web fonts.
+
+Example:
+
+In your `_layouts` -> `default.html` template file you have your __logo__ at the top of the page. You want to serve it from the CDN:
+
+    <object type="image/svg+xml" data="{{ '/img/logo.svg' | to_cdnurl }}">Project Name</object>
+
+When you _generate_ your site, the result is:
+
+    <object type="image/svg+xml" data="//xxxxxxxxxxxxx1.cloudfront.net/v100000/img/logo.svg">Project Name</object>
+
+Depending on the path given to the method, a different CDN host will be chosen using crc32. The final URL is put together like this:
+
+    //xxxxxxxxxxxxxN.cloudfront.net/BASE_URL/PREFIX+RELEASE/img/dog.jpg
+
+Note: http: is intentionally left off for protocol anonymous URLs.
+
+See the documentation in the plugin code for this method for more information.
+
+## Methods that begin with sub_
+
+Methods that begin with `sub_` do single or global regex string replacement on the input.
+
+You should use these methods sparingly if at all as it is better to use a `to_` method on the input on a per instance basis.
+
+### methods sub_baseurl and sub_absurl
+
+See the `to_baseurl` and `to_absurl` for basic workings of these methods. The only difference is that the `sub_` methods search through a whole block of input rather than just a short string, meaning multiple instances of _relative_ URLs can be effected.
+
+Example:
+
+One place the `sub_absurl` method is very handy is in your feed xml file. Use it on the `post.content` to make any _relative_ URLs _absolute_.
+
+    post.content (in Markdown):
+      Lorem ipsum dolor sit amet, consectetur adipisicing. ![Dog Image](/img/dog.jpg)
+
+    feed.xml:
+
+    {% for post in site.posts %}
+      <entry>
+        ...
+        <content type="html">{{ post.content | sub_absurl | xml_escape }}</content>
+      </entry>
+    {% endfor %}
+
+When you _generate_ your site, the result is:
+
+    <entry>
+      ...
+      <content type="html">
+        Lorem ipsum dolor sit amet, consectetur adipisicing. &lt;a href=&quot;http://domain.tld/img/dog.jpg&quot;&gt;
+      </content>
+    </entry>
+
+### method sub_imgurl
+
+Append image alternate size to images generated in templates such as sidebars and post asides.
+
+This is useful for when you need to show a thumbnail or smaller version of an image such as in a __Post Archive__.
+
+Q: Why not just hardcode the size in the template file?
+
+A: You could. However, if you are specifying a feature image for a post in the `YAML Front Matter` you will be using something like `page.image.src` in your sidebar template and so you can't insert the thumbnail size without using a regex. So moving that task into the plugin was deemed more __DRY__.
+
+Arguments:
+__size__ -  The string representing the image size. Recommended format is WidthPXxHeightPX.
+__hires__ - Boolean. Append @2x to the image name if true.
+
+Example:
+
+    <img src="{{ '/img/dog.jpg' | sub_imgurl }}" />
+
+When you _generate_ your site, the result is:
+
+    <img src="/img/dog_150x150.jpg" />
+
+Specify @size:
+
+    <img src="{{ '/img/dog.jpg' | sub_imgurl: '300x300' }}" />
+
+Result:
+
+    <img src="/img/dog_300x300.jpg" />
+
+Specify @size and @hires:
+
+    <img src="{{ '/img/dog.jpg' | sub_imgurl: '300x300', true }}" />
+
+Result:
+
+    <img src="/img/dog_300x300@2x.jpg" />
+
+### method sanitize_str
+
+Sanitize a string for use in URLs. Specifically, when you want to link to a __category__ or __tag__ name in your templates.
+
+Category or Tag names with spaces, capital letters, etc. will break your URLs. This method formats them correctly.
+
+Example:
+
+    web Development
+
+When you _generate_ your site, the result is:
+
+    web-development
 
 ## Author
 
--- url-helper-filter.rb
+++ url-helper-filter.rb
@@ -1,96 +1,97 @@
 module Jekyll
   module Filters
 
-    # The @get_ methods grabs/returns a value from _config.yml
+    # See the README for usage examples.
+
+    # The get_ methods grabs/returns a value from _config.yml.
+
+    # Internal: Get the 'url' variable value from _config.yml.
     def get_url
       url = @context.registers[:site].config['url']
     end
 
+    # Internal: Get the 'baseurl' variable value from _config.yml.
     def get_baseurl
       baseurl = @context.registers[:site].config['baseurl']
     end
 
-    # The @to_ methods transform a root-relative path into the path type
-    # of the method name.
+    # The to_ methods transform a root-relative path into the path
+    # type of the method name: to_<type>url.
 
-    # BASE URL: /img/dog.jpg to /blog/img/bog.jpg
+    # Public: Append the 'baseurl' variable to 'input'.
     def to_baseurl(input)
 
       # baseurl << input # wouldn't work, created a huge concat chain
       input = "#{get_baseurl}#{input}"
     end
 
-    # ABSOLUTE URL: /img/dog.jpg -> http://domain.tld/img/dog.jpg
-    #
-    # If _config.yml @baseurl is set that value will be inserted between
-    # the TLD and the relative path, e.g.:
-    # http://domain.tld/blog/img/dog.jpg
+    # Public: Append the 'url' variable to 'input'.
     def to_absurl(input)
 
       # url + baseurl << input
       input = "#{get_url}#{get_baseurl}#{input}"
     end
 
-    # Prepend a CDN 'virtual versioned' URL for any relative URL resource,
+    # Public: Append a CDN HTTP host name to a root-relative URL.
     #
-    # Example input value for a relative image resource:
-    # /img/dog.jpg
+    # Requires: Zlib
     #
-    # Example return value for an Amazon Cloudfront CDN URL:
-    # //xxx.cloudfront.net/BASE_URL/PREFIX+RELEASE/img/dog.jpg
+    # cdn_hosts - An array of up to 4 hosts, ordered from 0 to 3.
+    #             Order matters. Pos 0 is host 0, Pos 1 is host 1,
+    #             etc. Use Cloudfront hosts if you need SSL, or
+    #             CNAMEs otherwise.
     #
-    # Using crc32 to return consistent cross-machine numeric value of string.
-    # input.hash sucks and returns diff value for same string, even on same
-    # machine.
     #
-    # Using value of @input string to match with same CDN Host upon successive
-    # site regenerations to maintain caching across builds unless release id
-    # changes.
+    # release   - 6 digit integer. Default is today's date with
+    #             two-digit century, e.g., 2025/01/15, release:
+    #             250115.
     #
-    # Release id, if not set or null, the current day's date will be used
-    # with two digit century; given 2025/01/15, release will equal 250115
+    #             The current site version for use in 'virtual
+    #             versioned' URLs and an Origin Pull CDN setup
+    #             such as CloudFront. This method does not
+    #             require one to use 'invalidate' on every site
+    #             update and syncs all assets/files on your web
+    #             server with CDN URLs automatically with
+    #             Cloudfront, so you don't need to use S3.
     #
-    # Prefix. Release id is prefixed with this value, for catching in
-    # an .htaccess RewriteRule such as:
+    #             Only increment if EXISTING assets need to be
+    #             refreshed. Adding new assets to the current
+    #             cache does not require a new release number.
+    #
+    # prefix    - Release id is prefixed with this string. Useful
+    #             for catching in an .htaccess RewriteRule.
+    #             Default is 'v'.
+    #
+    # mode      - (development|production). The current working
+    #             state. Use 'development' when working locally,
+    #             offline or when you want to save bandwidth and
+    #             CDN costs. Use 'production' when shipping or
+    #             making your code live.
+    #
+    # Note: Store 'cdn_hosts', 'release', 'prefix' and 'mode' in
+    # a user defined hash named 'app' _config.yml. See the README
+    # for instructions on how to setup your _config.yml.
+    #
+    # Note: Using crc32 method to return consistent cross-machine
+    # numeric value of string; input.hash sucks and returns diff
+    # value for same string, even on same machine.
+    #
+    # Note: Using value of @input string to match with same CDN
+    # Host upon successive site regenerations to maintain caching
+    # across builds.
+    #
+    # Note: You will also need a RewriteRule in .htaccess to
+    # create the 'virtual version' match to the actual asset
+    # (assumes `prefix` is 'v'):
+    #
     # RewriteRule ^v[0-9]{6,6}/(.*)$ /$1 [L]
     #
-    # EXAMPLE _config.yml setup
-    # App Config ======================================================== #
-    # app:
-
-      # Release ================================================ #
-
-      # RELEASE
-      # The current site version for use in 'virtual versioned' URLs
-      # and an Origin Pull CDN setup such as CloudFront. This method
-      # does not require one to use 'invalidate' on every site update
-      # and syncs automatically with Cloudfront, so you don't need to
-      # use S3.
-      #
-      # Exactly 6 digit integer. Increment by 1 for every LIVE release.
-      #
-      # Release id, if not set or null, the current day's date will be
-      # used with two digit century; given 2025/01/15, release will
-      # equal 250115
-      # release: 100012
-
-      # Mode =================================================== #
-
-      # MODE
-      # Switches some URL paths to Relative, Absolute or CDN.
-      # Options: (development|production)
-      # mode: development
-
-      # CDN ==================================================== #
-
-      # CDN HOSTS
-      # Order matters. Pos 1 is host 0, Pos 2 is host 1 etc.
-      # Change to CNAME hosts if you don't need SSL.
-      # cdn_hosts:
-        # - xxxxxxxxxxxxx1.cloudfront.net
-        # - xxxxxxxxxxxxx2.cloudfront.net
-        # - xxxxxxxxxxxxx3.cloudfront.net
-        # - xxxxxxxxxxxxx4.cloudfront.net
+    # Note: You do not actually have to rename any files or
+    # folders to use 'virtual versioned' URLs. You only need to
+    # make sure your 'href' and 'src' attributes point to the
+    # virtual version URL. The method does exactly that for you.
+    #
+    # Returns: //xxxxxxxxxxxxxN.cloudfront.net/BASE_URL/PREFIX+RELEASE/img/dog.jpg
     def to_cdnurl(input)
 
       require 'zlib'
@@ -108,7 +109,11 @@ module Jekyll
         release = @context.registers[:site].time.strftime('%y%m%d')
       end
 
-      prefix = 'v'
+      prefix = @context.registers[:site].config['app']['prefix']
+
+      if !prefix
+        prefix = 'v'
+      end
 
       # Debugging
       # puts "\nInput: #{input}\nCDN Host: #{cdn_host}\nCDN Sub: #{cdn_sub}\nCDN Num: #{cdn_num}\nRelease: #{release}\nHash: #{hash}\n//#{cdn_host}#{get_baseurl}/#{prefix}#{release}#{input}\n"
@@ -121,26 +126,24 @@ module Jekyll
       end
     end
 
-    # Append image size to images generated in templates for use in
-    # sidebars, thumbnails, etc.
-    #
-    # Default usage:
-    # <img src="{{ '/img/dog.jpg' | sub_imgurl }}" />
-    #
-    # Result:
-    # <img src="/img/dog_150x150.jpg" />
-    #
-    # Specify @size:
-    # <img src="{{ '/img/dog.jpg' | sub_imgurl: '300x300' }}" />
-    #
-    # Result:
-    # <img src="/img/dog_300x300.jpg" />
-    #
-    # Specify @size and @hires:
-    # <img src="{{ '/img/dog.jpg' | sub_imgurl: '300x300', true }}" />
-    #
-    # Result:
-    # <img src="/img/dog_300x300@2x.jpg" />
+    # Public: Regex globally append the 'baseurl' variable to 'input'.
+    def sub_baseurl(input)
+      input.gsub(/(href|src)="\//, "\\1=\"#{get_baseurl}/")
+    end
+
+    # Public: Regex globally append the 'url' variable to 'input'.
+    def sub_absurl(input)
+
+      # Step 1: Prepend http: protocol to // URLs (protocol anonymous).
+      #
+      # Step 2: Prepend @url and @baseurl to Root-Relative URLs
+      # in templates to make an Absolute URL. Looks for ="/ in
+      # href or src tags, not directly followed by a / (look ahead)
+      input.gsub(/(href|src)="\/\//, "\\1=\"http://").gsub(/(href|src)="\/(?!\/)/, "\\1=\"#{get_url}#{get_baseurl}/")
+    end
+
+    # Public: Regex append image thumbnail sizes and optionally
+    # @2x to 'input'.
     def sub_imgurl(input, size = '150x150', hires = nil)
 
       if hires === true
@@ -150,27 +153,7 @@ module Jekyll
       input.sub(/\.(jpg|png|gif)/, "_#{size}#{hires}.\\1")
     end
 
-    # Prepend @baseurl to root-relative URLs in templates to make a
-    # Root-Base-Relative URL. Looks for ="/ in href or src tags
-    def sub_baseurl(input)
-      input.gsub(/(href|src)="\//, "\\1=\"#{get_baseurl}/")
-    end
-
-    def sub_absurl(input)
-
-      # Step 1: Prepend http: protocol to // URLs (protocol anonymous).
-
-      # Step 2: Prepend @url and @baseurl to Root-Relative URLs
-      # in templates to make an Absolute URL. Looks for ="/ in
-      # href or src tags, not directly followed by a / (look ahead)
-      input.gsub(/(href|src)="\/\//, "\\1=\"http://").gsub(/(href|src)="\/(?!\/)/, "\\1=\"#{get_url}#{get_baseurl}/")
-    end
-
-    # Sanitize for use in URL string.
-    #
-    # E.g.: web Development
-    #
-    # Result: web-development
+    # Public: Sanitize a string for use in a URL.
     def sanitize_str(input)
       input.gsub(/[^a-z0-9 -]+/, '').gsub(/\s/, '-').gsub(/-{2,}/, '-').downcase
     end
